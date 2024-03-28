c7b61c6b88d519e48fcc03bc208ddf3580aba209
Paul Sarando <psarando@cyverse.org>
2024-03-27 20:48:26 -0700

    CORE-1985 Turn on linter warnings for anonymous literal exports
    Set the `allowLiteral` flag to `false` for
    import/no-anonymous-default-export warnings,
    but allow anonymous object exports for now.

--- src/server/api/instantlaunches.js
+++ src/server/api/instantlaunches.js
@@ -10,7 +10,7 @@ import { handler as terrainHandler } from "./terrain";
 import * as auth from "../auth";
 import logger from "../logging";
 
-export default () => {
+const instantlaunches = () => {
     const api = express.Router();
 
     logger.info("************ Adding Instant Launch handlers **********");
@@ -193,3 +193,5 @@ export default () => {
 
     return api;
 };
+
+export default instantlaunches;
